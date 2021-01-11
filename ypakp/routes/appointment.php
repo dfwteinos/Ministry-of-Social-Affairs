8e30bf95409393030c4dfe506040f1ea8f736955
dfwteinos <dimfwteinos@gmail.com>
2021-01-12 00:40:22 +0200

    index.php and appointment

---
+++ ypakp/routes/appointment.php
@@ -0,0 +1,8 @@
+<?php
+
+include('../views/header.html');
+include('../views/appointment.html');
+// include('../templates/appointment/Medilab-bootstrap-5/Medilab/index.html')
+include('../views/footer.html');
+
+?>
---
+++ ypakp/routes/covprev.php
@@ -0,0 +1,9 @@
+<?php       
+
+include('../views/header.html');
+
+include('../views/covprev.html');
+
+include('../views/footer.html');
+
+?>
